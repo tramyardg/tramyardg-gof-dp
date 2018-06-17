183c355a6c5a8a54e3246a59514d40a399907a94
Raymart De Guzman <tramyardg@gmail.com>
2018-06-17 00:45:30 -0400

    #6 OMSAdapter to use the new OMS and adapter client

---
+++ src/main/java/com/tramyardg/dp/structural/adapter/AdapterClient.java
@@ -0,0 +1,18 @@
+package com.tramyardg.dp.structural.adapter;
+
+public class AdapterClient {
+
+    public static void main(String[] args) {
+	
+	OMSAdapter omsAdapter = new OMSAdapter();
+	omsAdapter.addItem(new Item("Medium Pizza", 13.99));
+	omsAdapter.addItem(new Item("Wine", 12.89));
+	omsAdapter.addItem(new Item("Beer", 6.99));
+	
+	omsAdapter.makePayment(new Payment("CASH", 20.00));
+	omsAdapter.makePayment(new Payment("CREDIT", 15.00));
+	omsAdapter.makePayment(new Payment("DEBIT", 10.00));
+	
+    }
+    
+}
