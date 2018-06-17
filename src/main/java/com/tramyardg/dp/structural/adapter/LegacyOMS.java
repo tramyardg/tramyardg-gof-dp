8f43dfd9c50f924468ed036385fccdf326236eae
Raymart De Guzman <tramyardg@gmail.com>
2018-06-17 00:18:19 -0400

    #6 adapter pattern old OMS client

--- src/main/java/com/tramyardg/dp/structural/adapter/LegacyOMS.java
+++ src/main/java/com/tramyardg/dp/structural/adapter/LegacyOMS.java
@@ -1,5 +1,29 @@
 package com.tramyardg.dp.structural.adapter;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * This old OMS accepts <b>XML</b> file as input.
+ * 
+ * @author tramyardg
+ *
+ */
 public class LegacyOMS {
 
+    private final Logger logger = Logger.getLogger(LegacyOMS.class.getName());
+
+    List<Item> cart = new ArrayList<>();
+    List<Payment> payments = new ArrayList<>();
+
+    public void addItem(Item itemXML) {
+	cart.add(itemXML);
+	logger.info(itemXML.getName() + " " + itemXML.getPrice() + " ");
+    }
+
+    public void makePayment(Payment paymentXML) {
+	payments.add(paymentXML);
+	paymentXML.pay();
+    }
 }
